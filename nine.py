5e7049341938226b08e2d49cbc6f3d32d85f0a12
Sabryna <sabrynarodrigues1804@gmail.com>
2022-07-18 14:24:01 -0300

    maior

---
+++ nine.py
@@ -0,0 +1,16 @@
+# WHAT IS THE BIGGEST NUMBER? - 18.07.2022
+def mai(*value):
+    print(f'\nAnalyzing the numbers...')
+    print(value)
+    print('.・。.・゜✭・.・✫・゜・。.')
+    maior = max(*value)
+    print(f'The highest value is {maior}.')
+    print('.・。.・゜✭・.・✫・゜・。.')
+
+
+
+
+# MAIN
+mai(9,7,8,5,3)
+mai(2,1)
+mai(23,45,100,345)
